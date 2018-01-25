35cdf8689072a9c6b799ee4d26922c5c60e63f79
mylcode <mylcode.ali@gmail.com>
2018-01-25 17:30:32 +0800

    补充宏定义

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -30,8 +30,15 @@
 
 - (void)testExample
 {
-    BOOL ret = macro_if_eq(6, 6)(1)(0);
-    XCTAssertTrue(ret, "XCTAssertTrue Fail");
+    XCTAssertTrue(macro_argcount(@1)==1, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2)==2, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2,@3)==3, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2,@3,@4)==4, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2,@3,@4,@5)==5, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2,@3,@4,@5,@6)==6, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2,@3,@4,@5,@6,@7)==7, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2,@3,@4,@5,@6,@7,@8)==8, @"not equal");
+    XCTAssertTrue(macro_argcount(@1,@2,@3,@4,@5,@6,@7,@8,@9)==9, @"not equal");
 }
 
 @end
